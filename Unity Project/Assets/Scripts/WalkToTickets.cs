8536e89c4a922eaf0cef3b4f140d5404bab35a62
Ahmed Khedr <ahmedkhedr2910@gmail.com>
2023-11-25 03:35:51 +0100

    ...

--- Unity Project/Assets/Scripts/WalkToTickets.cs
+++ Unity Project/Assets/Scripts/WalkToTickets.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class WalkToTickets : MonoBehaviour
 {
     public GameObject movieChoiceCanvas;
+    public PlayableDirector timeline;
 
     void Update()
     {
         if (transform.position.z < 1)
         {
             movieChoiceCanvas.SetActive(true);
+        }
+        if (timeline.state != PlayState.Playing)
+        {
+            movieChoiceCanvas.SetActive(false);
         }
     }
 }
